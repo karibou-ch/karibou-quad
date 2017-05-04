b6dda099383064c42881cdb829f2c4c4dc553522
godsend <jcavat@gmail.com>
2017-05-04 14:18:43 +0200

    Improve data extraction into multiple files

--- prototype.py
+++ prototype.py
@@ -1,198 +1,82 @@
 import json
 import csv
-from itertools import *
 from functools import *
-
-
-def vendors_list(transactions):
-    vendors = []
-    for t in transactions:
-        for i in t['items']:
-            vendors.append(i['vendor'])
-
-    return list(set(vendors))
-
-def customers_list(transactions):
-    customers = []
-    for t in transactions:
-        if isinstance(t['customer']['id'], dict):
-            customers.append(t['customer']['id']['floatApprox'])
-        else:
-            customers.append(t['customer']['id'])
-
-    return list(set(customers))
+from itertools import *
 
 def is_problematic(item):
     return ('issue' in item.keys() and item['issue'] != 'issue_no_issue') or item['status'] == 'failure'
 
-
-def issues_details(transactions, vendor):
-    stat = []
-    for t in transactions:
-        for i in t['items']:
-            if i['vendor'] == vendor:
-                if 'issue' in i.keys() and i['issue'] != 'issue_no_issue':
-                    stat.append(i['issue'])
-                elif i['status'] == 'failure':
-                    stat.append('failure')
-    return stat
-
-
-def stat_customer(transactions, customer_id):
-    stat = []
-    for t in transactions:
-        if isinstance(t['customer']['id'], dict) and t['customer']['id']['floatApprox'] == customer_id or \
-            t['customer']['id'] == customer_id:
-            items = t['items']
-            for item in items:
-                item['date'] = t['shipping']['when']
-            stat.append(items)
-
-    amount = 0
-    nb_item = 0
-    issue = []
-    items = []
-    for item in reduce( lambda x, y: x+y, stat ):
-        amount += item['finalprice']
-        nb_item += 1
-
-        if 'issue' in item.keys() and item['issue'] != 'issue_no_issue':
-            issue.append( { item['vendor']: item['issue'] })
-        elif item['status'] == 'failure':
-            issue.append( { item['vendor']: 'failure' })
-
-        items.append(item)
-        #print(item)
-
-    rep = {}
-    rep['nb_transactions'] = len(stat)
-    rep['nb_issue'] = len(issue)
-    rep['amount'] = amount
-    rep['mean_amount'] = amount / rep['nb_transactions']
-    rep['issue'] = issue
-    rep['item_percent_issue'] = rep['nb_issue'] / nb_item * 100
-
-    rep['matrix'] = groupby(sorted(items, key=lambda i: i['vendor']), key=lambda i: i['vendor'])
-
-    return rep
-
-def stat_vendor(transactions, vendor):
-    stat = []
-    for t in transactions:
-        for i in t['items']:
-            if i['vendor'] == vendor:
-                issue = 0
-                if is_problematic(i):
-                    issue += 1
-
-                stat.append( { 'nb_transaction': 1, 'qty': i['qty'], 'price': i['price'], 'finalprice': i['finalprice'], 'nb_issue': issue, 'transaction_issue': 1 if issue > 0 else 0  } )
-
-    rep = {}
-    rep['nb_transaction'] = sum( [s['nb_transaction'] for s in stat] )
-    rep['amount'] = sum( [s['finalprice'] for s in stat] )
-    qty = sum( [s['qty'] for s in stat] )
-    rep['qty'] = qty
-    rep['nb_item_issue'] = sum( [s['nb_issue'] for s in stat] )
-    rep['nb_transaction_issue'] = sum( [s['transaction_issue'] for s in stat] )
-    rep['item_percent_issue'] = rep['nb_item_issue'] / rep['nb_transaction'] * 100
-    rep['transactions_percent_issue'] = rep['nb_transaction_issue'] / rep['nb_transaction'] * 100
-    rep['qty_percent_issue'] = rep['nb_transaction_issue'] / qty * 100
-    rep['vendor'] = vendor
-
-    if qty != 0:
-        rep['mean_items'] = rep['amount'] / qty
-    else:
-        rep['mean_items'] = 0
-
-    return rep
-
-
-def show_customers_stat(transactions):
-    exports_sc = []
-    exports_cust = []
-    for c in customers_list(transactions):
-        print("*" * 100)
-        print(c)
-        stat = stat_customer(transactions, c)
-        for k, v in stat.items():
-            if k != 'matrix':
-                print("\t {0: <30}".format(k, ": "), v)
-
-        print("\t", "*" * 50)
-        total_issue = 0
-        total_amount = 0
-        total_transaction = 0
-        for k, items in stat['matrix']:
-            print("\t", k)
-            amount = 0
-            nb_transactions = 0
-            nb_issues = 0
-            for item in items:
-                nb_transactions += 1
-                amount += item['finalprice']
-                nb_issues += 1 if is_problematic(item) else 0
-
-            exports_sc.append( {'customer': c, 'vendors': k, 'nb_transactions': nb_transactions, 'amount': amount, 'nb_issues': nb_issues, 'percent_issue': nb_issues/nb_transactions*100 } )
-
-            total_issue += nb_issues
-            total_amount += amount
-            total_transaction += nb_transactions
-
-
-            print("\t\tamout: ", amount)
-            print("\t\tnb_transactions: ", nb_transactions)
-            print("\t\tnb_issues: ", nb_issues)
-
-
-
-        exports_cust.append( {'customer': c, 'nb_issues': total_issue, 'nb_transactions': total_transaction, 'amount': total_amount, 'percent_issue': total_issue/total_transaction*100 } )
-
-    keys = exports_sc[0].keys()
-    with open('customers_vendors.csv', 'w') as output:
+def export_to_csv(dictionnary, filename):
+    keys = dictionnary[0].keys()
+    with open(filename, 'w') as output:
         dict_writer = csv.DictWriter(output, keys)
         dict_writer.writeheader()
-        dict_writer.writerows(exports_sc)
-
-    keys = exports_cust[0].keys()
-    with open('customers.csv', 'w') as output:
-        dict_writer = csv.DictWriter(output, keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(exports_cust)
-
-def show_vendors_stat(transactions):
-    stat = []
-    for v in vendors_list(transactions):
-        stat.append(stat_vendor(transactions, v))
-
-
-    for s in reversed(sorted(stat, key=lambda s: s['item_percent_issue'])):
-        print("*" * 100)
-        print(s['vendor'])
-        for k, v in s.items():
-            if k != 'vendor':
-                print("\t {0: <30}".format(k), "{0: <20}".format(v))
-
-        if len(issues_details(transactions, s['vendor'])) > 0:
-            print("\t", "*" * 50)
-            for k, value in groupby(sorted(issues_details(transactions, s['vendor']))):
-                print("\t", k, len(list(value)))
-            print("\t", "*" * 50)
-
-
-    keys = stat[0].keys()
-    with open('vendors.csv', 'w') as output:
-        dict_writer = csv.DictWriter(output, keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(reversed(sorted(stat, key=lambda s: s['transactions_percent_issue'])))
-
+        dict_writer.writerows(dictionnary)
 
 with open('./tests/data/orders.json') as json_file:
     transactions = json.load(json_file)
 
+    def add_transaction_info_on_item(item, transaction):        
+        customer = transaction['customer']
+        customer = customer['id']['floatApprox'] if isinstance(customer['id'], dict) else customer['id']
+        oid = transaction['oid']
+        item.update({ 'customer': customer })
+        item.update({ 'id_transaction': oid })
+        return item
 
-    show_vendors_stat(transactions)
+    for t in transactions:
+        t['items'] = [ add_transaction_info_on_item(i, t) for i in t['items'] ]
 
-    for _ in range(4):
-        print("*" * 100)
+    items = [ t['items'] for t in transactions]    
 
-    show_customers_stat(transactions)
+    #items = reduce( lambda x, y: x+y, items )
+
+    def reduce_transaction(transaction):
+        def reduce_item(item):
+            return { 
+                'finalprice': item['finalprice'],
+                'vendor': item['vendor'],
+                'customer': item['customer'],
+                'is_problematic': 1 if is_problematic(item) else 0,
+                'id_transaction': item['id_transaction']
+            }
+
+        mapped_items = [ reduce_item(item) for item in transaction ]
+        return mapped_items
+
+    mapped_items = [ reduce_transaction(i) for i in items ]
+    mapped_items = reduce( lambda x, y: x+y, mapped_items )
+
+    export_to_csv(mapped_items, 'transactions.csv')
+
+    vendors_details = []
+    for k, mis in groupby(sorted(mapped_items, key=lambda mi: mi['vendor']), key=lambda mi: mi['vendor']):
+        mis = list(mis)
+
+        nb_issues = sum([ mi['is_problematic'] for mi in mis ])
+        nb_transactions = len(list(mis))
+        amount = sum([ mi['finalprice'] for mi in mis ])
+        nb_customers = len( set( [ mi['customer'] for mi in mis ] ))
+
+        vendors_details.append( { 'vendor': k, 'nb_issues': nb_issues, 'nb_transactions': nb_transactions, 'amount': amount, 'nb_customers': nb_customers } )
+
+    export_to_csv(vendors_details, 'vendors2.csv')
+
+
+    customers_details = []
+    for k, mis in groupby(sorted(mapped_items, key=lambda mi: mi['customer']), key=lambda mi: mi['customer']):
+        mis = list(mis)
+
+        nb_issues = sum([ mi['is_problematic'] for mi in mis ])
+        nb_transactions = len(list(mis))
+        amount = sum([ mi['finalprice'] for mi in mis ])
+        nb_vendors = len( set( [ mi['vendor'] for mi in mis ] ))
+
+        customers_details.append( { 'customer': k, 'nb_issues': nb_issues, 'nb_transactions': nb_transactions, 'amount': amount, 'nb_vendors': nb_vendors } )
+
+    export_to_csv(customers_details, 'customers2.csv')
+
+
+
+
+
--- script.r
+++ script.r
@@ -1,5 +1,12 @@
-vec <-read.csv("vendors.csv", skip=2, header=FALSE)
+vec <-read.csv("stats.csv", header=TRUE)
+
+aggregate(cbind(finalprice, is_problematic), by=list(vendor, customer), FUN=sum, na.rm = TRUE)
 
 
+
+#ggregate(cbind(finalprice, is_problematic), by=list(vendor, customer), FUN=sum, na.rm = TRUE)
+#
+#
+#
 #stat <-read.csv("stat.txt", sep=";", skip=2, header=FALSE, row.names = NULL)
 #pop = stat$V2[stat$V1 == 'pop'] / 1e06
