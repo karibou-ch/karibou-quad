fb5efaaa3f628b613b03990ae1db654a5af19687
godsend <jcavat@gmail.com>
2017-05-04 23:04:51 +0200

    Export vendors plots to jpg

--- scripts/prototype.py
+++ scripts/prototype.py
@@ -3,6 +3,10 @@ import csv
 from functools import *
 from itertools import *
 from dateutil.parser import parse
+import datetime
+import matplotlib.pyplot as plt
+
+
 
 def is_problematic(item):
     return ('issue' in item.keys() and item['issue'] != 'issue_no_issue') or item['status'] == 'failure'
@@ -137,5 +141,55 @@ with open('../tests/data/orders.json') as json_file:
 
 
 
+    # Vendors vs Time details ----------------------------------------------------------------------------------------------------
+    vendors_time_details = []
+    for vendor, items in groupby(sorted(mapped_items, key=lambda mi: mi['vendor']), key=lambda mi: mi['vendor']):
+        items = list(items)
+        # transform str_date to date
+        for i in items:
+            i['date'] = datetime.datetime.strptime(i['date'], '%d.%m.%Y').date()
+
+        acc = []
+        acc2  = []
+        axis = []
+        # group by year
+        for year, items in groupby(sorted(items, key=lambda mi: mi['date']), key=lambda mi: mi['date'].year):
+            items = list(items)
+            for month, items in groupby(sorted(items, key=lambda mi: mi['date']), key=lambda mi: mi['date'].month):
+                items = list(items)
+
+                nb_issues = sum([ mi['is_problematic'] for mi in items ])
+                nb_transactions = len(list(items))
+                amount = sum([ mi['finalprice'] for mi in items ])
+                price_diff = sum([ mi['price_diff'] for mi in items ])
+                nb_vendors = len( set( [ mi['vendor'] for mi in items ] ))
+                nb_customers = len( set( [ mi['customer'] for mi in items ] ))
+
+                acc.append( nb_transactions)
+                acc2.append(nb_issues)
+                axis.append('1.' + str(month) + '.' + str(year))
+
+                #print("\t", '1.' + str(month) + '.' + str(year), (nb_issues, nb_transactions, amount, price_diff, nb_vendors, nb_customers, nb_issues/nb_transactions*100))
+
+        fig, ax1 = plt.subplots()
+
+        plt.xticks(rotation=70)
+        plt.xticks(range(len(axis)), axis)
+
+        ax1.plot(acc)
+
+        ax2 = ax1.twinx()
+        ax2.set_ylim([0,20])
+        ax2.plot(acc2, color='r')
+        ax2.tick_params('y', colors='r')
+
+        fig.tight_layout()
+        plt.title(vendor)
+        plt.savefig('./out/' + vendor + '.png')
+        plt.clf()
+        plt.close()
+
+
+    #export_to_csv(vendors_time_details, 'vendors_time.csv')
 
 
