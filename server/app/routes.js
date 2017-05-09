2eed73830f12e383dc9e3ba97c3422b656ac75a4
godsend <jcavat@gmail.com>
2017-05-09 15:03:55 +0200

    Add score and list vendors

--- server/app/routes.js
+++ server/app/routes.js
@@ -5,7 +5,30 @@ var Transactions = connectionsubject.model('', {}, 'orders');
 module.exports = function(app) {
 
 
-    app.get('/vendor/:id?', (req, res) => {
+    app.get('/list/vendors', (req, res) => {
+
+        Transactions
+            .aggregate([
+                {
+                    $unwind: '$items'
+                },
+                {
+                    $project: {'items.vendor': 1}
+                },
+                {
+                    $group: {_id: '$items.vendor'}
+                }
+            ],
+
+            (err, subjectDetails) =>  { 
+                if (err) {
+                    res.send(err);
+                }
+                res.json(subjectDetails.map( v => v['_id'] ));
+            })
+    });
+
+    app.get('/vendors/:id?', (req, res) => {
 
         const searchVendorId = req.params.id === undefined ? {} : { 'items.vendor': req.params.id };
 
@@ -60,9 +83,51 @@ module.exports = function(app) {
                                     issue_missing_product: '$issue_missing_product',
                                     issue_wrong_product_quality_failure: '$issue_wrong_product_quality_failure',
                                     issue_wrong_product_quality_fulfilled: '$issue_wrong_product_quality_fulfilled',
-                                    nb_items: '$nb_items'
+                                    nb_items: '$nb_items',
+                                    score: {
+                                        $add: [
+                                            { $multiply: ['$issue_missing_product', 1] }, 
+                                            { $multiply: ['$issue_wrong_product_quality_failure', 2] }, 
+                                            { $multiply: ['$issue_wrong_product_quality_fulfilled', 4] }
+                                        ]
+                                    },
+                                    score_rate: {
+                                        $multiply: [
+                                        {
+                                            $divide: [ {
+                                            $add: [
+                                                { $multiply: ['$issue_missing_product', 1] }, 
+                                                { $multiply: ['$issue_wrong_product_quality_failure', 2] }, 
+                                                { $multiply: ['$issue_wrong_product_quality_fulfilled', 4] }
+                                            ]},
+                                            '$nb_items']
+                                        }, 100]
+                                    }
                                 } 
                             }
+                        }
+                    },
+                    {
+                        $project: {
+                            customers: 1,
+                            issue_missing_product: 1,
+                            issue_wrong_product_quality_failure: 1,
+                            issue_wrong_product_quality_fulfilled: 1,
+                            nb_items: 1,
+                            customers_details: 1,
+                            score: {
+                                $sum: '$customers_details.score'
+                            }
+                        }
+                    },
+                    {
+                        $addFields: {
+                            score_rate: { $multiply: [{$divide: ['$score', '$nb_items']}, 100] }
+                        }
+                    },
+                    {
+                        $sort: {
+                            'score_rate': -1
                         }
                     }
                 ],
@@ -70,23 +135,7 @@ module.exports = function(app) {
                     if (err) {
                         res.send(err);
                     }
-                    res.json(_
-                        .chain(subjectDetails)
-                        .map( 
-                            i => { 
-                                i['score'] = i['issue_missing_product'] + i['issue_wrong_product_quality_fulfilled']*2 + i['issue_wrong_product_quality_fulfilled']*4;
-                                i['score_rate'] = i['score'] / i['nb_items'] * 100;
-                                _.map(i['customers_details'], ( i => {
-                                    i['score'] = i['issue_missing_product'] + i['issue_wrong_product_quality_fulfilled']*2 + i['issue_wrong_product_quality_fulfilled']*4;
-                                    i['score_rate'] = i['score'] / i['nb_items'] * 100;
-                                    return i;
-                                }));
-                                return i;
-                            }
-                        )
-                        .sortBy( i => i['score_rate'])
-                        .reverse()
-                    );
+                    res.json(subjectDetails);
                 }
         );
 
@@ -141,7 +190,7 @@ module.exports = function(app) {
                 }
         );
     });
-    app.get('/transaction', (req, res) => {
+    app.get('/transactions', (req, res) => {
         // use mongoose to get all nerds in the database
         Transactions
             .aggregate( 
