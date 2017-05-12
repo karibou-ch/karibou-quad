dfd266546966f7dc14feccd644783d4b7f581974
godsend <jcavat@gmail.com>
2017-05-12 15:57:17 +0200

    Change colors

--- client/src/app/charts/barchart/barchart.component.ts
+++ client/src/app/charts/barchart/barchart.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, Input, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
 import * as d3 from 'd3';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-barchart',
@@ -76,7 +77,8 @@ export class BarchartComponent implements OnInit {
     // update scales & axis
     this.xScale.domain(this.data.map(d => d[0]));
     this.yScale.domain([0, d3.max(this.data, d => d[1])]);
-    this.colors.domain([0, this.data.length-1]);
+    //this.colors.domain([0, this.data.length-1]);
+    this.colors.domain([0, 1]);
     this.xAxis.transition().call(d3.axisBottom(this.xScale));
     this.yAxis.transition().call(d3.axisLeft(this.yScale));
 
