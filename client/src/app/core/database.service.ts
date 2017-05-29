39ec9ff759f5fc23151377b82e6f3ea08875b880
jcavat <jcavat@gmail.com>
2017-05-29 12:41:31 +0200

    Add routes header functions documentation

--- client/src/app/core/database.service.ts
+++ client/src/app/core/database.service.ts
@@ -10,18 +10,32 @@ export class DatabaseService {
 
   constructor(private http: Http) { }
 
+  /**
+   * Returns vendor statistics
+   * @param id - the vendor's id
+   * @returns {Observable<any>} - a vendor statistics in an observable object
+   */
   vendor(id: string): Observable<any> {
     return this.http.get(url + 'vendors/' + id).map(
       (r: Response) => r.json()[0]
     );
   }
 
+  /**
+   * Returns all vendors statistics
+   * @returns {Observable<any[]>} - a list of vendors statistics in an observable list object
+   */
   vendors(): Observable<any[]> {
     return this.http.get(url + 'vendors/').map(
       (r: Response) => r.json()
     );
   }
 
+  /**
+   * Returns vendor statistics grouped by date
+   * @param id - the vendor's id
+   * @returns {Observable<any>} - a vendor statistics grouped by date
+   */
   issues_per_dates(id: string) {
     return this.http.get(url + `vendors/${id}/date`).map(
       (r: Response) => r.json()
